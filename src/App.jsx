7a4a93f99a01275f07c581aa9ee405e359104c84
TLPLTSzft <tlpltszft@gmail.com>
2023-01-06 23:27:45 +0100

    1. Feladat c.), d.), e.) 2. Feladat a.) 3. Feladat a.)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [teendok, setTeendok] = useState([]);
+  const [feladat, setFeladat] = useState("");
+  const feladatHozzaadasa = () => {
+    teendok.push(feladat);
+    console.log(teendok);
+  };
   return (
     <div>
+      <h1>Teendők</h1>
+      <section>
+        <h2>Feladat hozzáadása</h2>
+        <div>
+          <label>Feladat:</label>
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder='Feladat'
+            value={feladat}
+            onInput={e => { setFeladat(e.target.value); }}
+          />
+          {/* <input type="text" placeholder='Feladat' value={feladat} onInput={e => { setFeladat(e.target.value); }} /> */}
+        </div>
+        <button onClick={() => feladatHozzaadasa()}>Hozzáad</button>
+      </section>
+      <section>
+        <h2>Teendők</h2>
+        <div>
 
+        </div>
+
+      </section>
     </div>
   );
 }
